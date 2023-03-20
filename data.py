b5bf5bdab7a0546686bece42ae01724a96ee9861
mchen30 <29552524+mchen30@users.noreply.github.com>
2023-03-20 10:36:30 +0800

    extract winners

--- data.py
+++ data.py
@@ -1,8 +1,9 @@
 import json
 import re
+import unidecode
 
-gg2013 = json.load(open('../gg2013.json'))
-# gg2015 = json.load(open('../gg2015.json'))
+gg2013 = json.load(open('./gg2013.json'))
+# gg2015 = json.load(open('./gg2015.json'))
 
 
 def clean(data):
@@ -10,17 +11,19 @@ def clean(data):
         sent = tweet['text'].strip().split()
         tweet['hashtag'] = []
         new_sent = []
-        for word in sent:
+        for i, word in enumerate(sent):
             if word.startswith('#'):
                 # store hashtags separately
-                tweet['hashtag'].append(word[1:])
+                tweet['hashtag'].append(word[1:].lower())
             elif word == '&amp;':
                 # replace & with and
                 new_sent.append('and')
+            elif word.startswith('@') and i > 0 and sent[i - 1] == 'RT':
+                new_sent = []
             else:
                 # remove punctuation and case
                 new_sent.append(word.lower())
-        tweet['text'] = re.sub(r'[^\w\s]', '', " ".join(new_sent))
+        tweet['text'] = unidecode.unidecode(re.sub(r'[^\w\s]', '', " ".join(new_sent)))
 
 
 clean(gg2013)
--- process.py
+++ process.py
@@ -1,4 +1,5 @@
 from data import *
+import numpy as np
 import pandas as pd
 
 
@@ -7,9 +8,9 @@ def look_forward(sent, ind, start=None, end=None):
     for length in range(1, len(sent) - ind):
         if start is None and end is None:
             ngrams.append(sent[ind+1: ind+length+1])
-        elif start is not None and sent[ind+1] == start:
+        elif start is not None and len(start) <= length and sent[ind+1: ind+len(start)+1] == start:
             ngrams.append(sent[ind+1: ind+length+1])
-        elif end is not None and sent[ind+length+1] == end:
+        elif end is not None and len(end) <= length and sent[ind+length+1-len(end):ind+length+1] == end:
             ngrams.append(sent[ind+1: ind+length+1])
     return ngrams
 
@@ -19,9 +20,9 @@ def look_backward(sent, ind, start=None, end=None):
     for length in range(1, ind+1):
         if start is None and end is None:
             ngrams.append(sent[ind-length: ind])
-        elif start is not None and sent[ind-length] == start:
+        elif start is not None and len(start) <= length and sent[ind-length: ind-length+len(start)] == start:
             ngrams.append(sent[ind - length: ind])
-        elif end is not None and sent[ind] == end:
+        elif end is not None and len(end) <= length and sent[ind-len(end):ind] == end:
             ngrams.append(sent[ind - length: ind])
     return ngrams
 
@@ -37,6 +38,24 @@ def unique_ngrams(ngrams_lst):
         cnt.append([uni_ngram, ngrams_lst.count(uni_ngram)])
     sorted_cnt = sorted(cnt, key=lambda x: x[1], reverse=True)
     return sorted_cnt
+
+
+def find_inter(ngrams_lsts):
+    inter_cnt = []
+    sorted_cnts = []
+    for lst in ngrams_lsts:
+        sorted_cnts.extend(unique_ngrams(lst))
+    cand, freq = zip(*sorted_cnts)
+    cand = np.asarray([' '.join(c) for c in cand])
+    uniq_cand = list(set(cand))
+    for c in uniq_cand:
+        idxs = np.argwhere(cand == c)
+        if len(idxs) >= len(ngrams_lsts):
+            cnt = 0
+            for idx in idxs:
+                cnt += freq[idx.item()]
+            inter_cnt.append([c.split(' '), cnt])
+    return inter_cnt
 
 
 def is_Sublist(l, s):
@@ -92,7 +111,22 @@ def remove_all_sublists(sorted_ca):
     return [x[0] for x in sorted_ca]
 
 
-# extract host, equal weights for all methods
+def untie(sorted_ca):
+    if len(sorted_ca) == 0:
+        return []
+    freq = sorted_ca[0][1]
+    max_len = 0
+    best = None
+    idx = 0
+    while idx < len(sorted_ca) and sorted_ca[idx][1] == freq:
+        if len(sorted_ca[idx][0]) > max_len:
+            best = sorted_ca[idx][0]
+            max_len = len(sorted_ca[idx][0])
+        idx += 1
+    return [best, freq]
+
+
+'''# extract host, equal weights for all methods
 ca_set = []
 for tweet in gg2013:
     sent = tweet['text'].split()
@@ -149,17 +183,17 @@ for tweet in gg2013:
     for i, word in enumerate(sent):
         # best xxx goes to
         if word == 'goes' and i+1 < len(sent) and sent[i+1] == 'to':
-            ca_set_awards += look_backward(sent, i, start='best')
+            ca_set_awards += look_backward(sent, i, start=['best'])
         # xxx won best xxx
         elif word == 'won' and i > 0 and sent[i-1] == 'has':
-            ca_set_awards += look_forward(sent, i-1, start='best')
+            ca_set_awards += look_forward(sent, i-1, start=['best'])
         elif word == 'won':
-            ca_set_awards += look_forward(sent, i, start='best')
+            ca_set_awards += look_forward(sent, i, start=['best'])
         # best xxx - *winner
         # elif word == '-':
-        #     ca_set_awards += look_backward(sent, i, start='best')
+        #     ca_set_awards += look_backward(sent, i, start=['best'])
 
-sorted_ca_awards = unique_ngrams(ca_set_awards)[:90] #select only top eighty
+sorted_ca_awards = unique_ngrams(ca_set_awards)[:90]
 sorted_ca_awards = remove_duplicate_sublist(sorted_ca_awards)
 
 # remove all sublists at this point, re-rank based on occurrence frequency
@@ -174,9 +208,11 @@ for x in ca_awards:
             n += 1
     ca_freq_awards.append([x, n])
 
-sorted_ca_freq_awards = sorted(ca_freq_awards, key=lambda x :x[1], reverse=True)
+sorted_ca_freq_awards = sorted(ca_freq_awards, key=lambda x :x[1], reverse=True)'''
 
 # Print list of awards:
+
+
 awards = [['best', 'motion', 'picture', 'drama'],
           ['best', 'motion', 'picture', 'comedy', 'or', 'musical'],
           ['best', 'actor', 'in', 'a', 'motion', 'picture', 'drama'],
@@ -187,7 +223,6 @@ awards = [['best', 'motion', 'picture', 'drama'],
           ['best', 'screenplay'],
           ['best', 'original', 'score'],
           ['best', 'original', 'song', 'motion', 'picture'],
-          ['best', 'song'],
           ['best', 'animated', 'feature', 'film'],
           ['best', 'foreign', 'film'],
           ['best', 'tv', 'series', 'drama'],
@@ -198,53 +233,95 @@ awards = [['best', 'motion', 'picture', 'drama'],
           ['best', 'actor', 'in', 'a', 'tv', 'series', 'comedy', 'or', 'musical'],
           ['best', 'actress', 'in', 'a', 'tv', 'series', 'comedy', 'or', 'musical'],
           # miniseries/tv movie should map to miniseries or tv movie
-          ['best', 'actor', 'in', 'a', 'miniseries', 'tv', 'movie'],
-          ['best', 'actress', 'in', 'a', 'miniseries', 'tv', 'movie'],
+          ['best', 'actor', 'in', 'a', 'miniseries', 'or', 'tv', 'movie'],
+          ['best', 'actress', 'in', 'a', 'miniseries', 'or', 'tv', 'movie'],
           ['best', 'supporting', 'actor', 'in', 'a', 'tv', 'series'],
           ['best', 'supporting', 'actress', 'in', 'a', 'tv', 'series'],
           ['best', 'miniseries', 'or', 'tv', 'movie']]
 
-# xxxx is presenting AWARD
+
 
 # xxx (has) won AWARD
 # xxx has won the golden globe for AWARD
 # xxx ... for her golden globe win as AWARD
 
-# someone presents AWARD to xxx
 
-sorted_ca_freq_winners = []
-sorted_ca_freq_presenters = []
-sorted_ca_freq_nominees = []
+n_awards = len(awards)
+sorted_ca_freq_winners = [None] * n_awards
+sorted_ca_freq_presenters = [None] * n_awards
+sorted_ca_freq_nominees = [None] * n_awards
 
 for i, award in enumerate(awards):
-    # extract award names
+    # get award names
     award_str = ' '.join(award)
     ca_set_winner = []
+    ca_set_presenter = []
     for tweet in gg2013:
         sent = tweet['text'].split()
-        for i, word in enumerate(sent):
-            sent_str = ' '.join(sent[i:])
-            # AWARD: xxx
-            if award_str in sent_str and i + len(award) < len(sent):
-                ca_set_winner += look_forward(sent, i+len(award))
-            # AWARD goes to xxx
-            elif award_str in sent_str and i + len(award) + 2 < len(sent) and sent[i + len(award)] == 'goes' and sent[i + len(award) + 1] == 'to':
-                ca_set_winner += look_forward(sent, i+len(award)+2)
-            # xxx - #gg winner for AWARD
-            elif award_str in sent_str and i > 2:
-                ca_set_winner += look_backward(sent, i - 2)
-            # winner for AWARD is xxx
-            elif award_str in sent_str and i >= 2 and sent[i-1] == 'for' and sent[i-2] == 'winner':
-                ca_set_awards += look_forward(sent, i, start='best')
+        for j, word in enumerate(sent):
+            sent_str = ' '.join(sent[j: j + len(award)])
+            if award_str == sent_str:
+                # AWARD goes to xxx
+                if j + len(award) + 2 < len(sent) and sent[j + len(award)] == 'goes' and sent[j + len(award) + 1] == 'to':
+                    ca_set_winner += look_forward(sent, j + len(award) - 1, start=['goes', 'to'])[2:]
+                # AWARD is awarded to xxx
+                elif j + len(award) + 3 < len(sent) and sent[j + len(award)] == 'is' and sent[j + len(award) + 1] == 'awarded' and sent[j + len(award) + 2] == 'to':
+                    ca_set_winner += look_forward(sent, j + len(award) - 1, start=['is', 'awarded', 'to'])[3:]
+                # AWARD: xxx
+                elif j + len(award) < len(sent):
+                    ca_set_winner += look_forward(sent, j + len(award) - 1)
+                # winner for AWARD is xxx
+                elif j >= 2 and sent[j - 1] == 'for' and sent[j - 2] == 'winner' and len(sent) > j + len(award) + 1 and sent[j + len(award)] == 'is':
+                    ca_set_winner += look_forward(sent, j + len(award) - 1, start=['is'])[1:]
+                # AWARD is xxx
+                elif len(sent) > j + len(award) and sent[j + len(award)] == 'is':
+                    ca_set_winner += look_forward(sent, j + len(award) - 1, start=['is'])[1:]
+                # someone presents AWARD to xxx
+                elif j >= 2 and len(sent) > j + len(award) and sent[j + len(award)] == 'to' and sent[j - 1] == 'presents':
+                    ca_set_winner += look_forward(sent, j + len(award) - 1, start=['to'])[1:]
+                # xxx for AWARD
+                elif j >= 2 and sent[j - 1] == 'for':
+                    ca_set_winner += look_backward(sent, j, end=['for'])[:-1]
+                # xxx wins/won/has won AWARD
+                elif j >= 2 and sent[j - 1] == 'wins':
+                    ca_set_winner += look_backward(sent, j, end=['wins'])[:-1]
+                elif j >= 2 and sent[j - 1] == 'won':
+                    ca_set_winner += look_backward(sent, j, end=['won'])[:-1]
+                elif j >= 3 and sent[j - 1] == 'won' and sent[j - 2] == 'has':
+                    ca_set_winner += look_backward(sent, j, end=['has', 'won'])[:-2]
+                # xxx wins/won the #goldenglobe for AWARD
+                elif j >= 4 and sent[j - 1] == 'for' and sent[j - 2] == 'goldenglobe' and sent[j - 3] == 'the' and sent[j - 4] == 'wins':
+                    ca_set_winner += look_backward(sent, j, end=['wins', 'the', 'goldenglobe', 'for'])[:-4]
+                elif j >= 4 and sent[j - 1] == 'for' and sent[j - 2] == 'goldenglobe' and sent[j - 3] == 'the' and sent[j - 4] == 'won':
+                    ca_set_winner += look_backward(sent, j, end=['won', 'the', 'goldenglobe', 'for'])[:-4]
+                # xxx - #goldenglobe winner for AWARD
+                elif j >= 2 and sent[j - 1] == 'for' and sent[j - 2] == 'winner':
+                    ca_set_winner += look_backward(sent, j, end=['goldenglobe', 'winner', 'for'])[:-3]
+                # congrat/congrats to xxx for her/his golden globe win as AWARD
+                elif j > 6 and sent[j - 1] == 'as' and sent[j - 2] == 'win' and sent[j - 3] == 'globe' and sent[j - 4] == 'golden' and sent[j - 5] == 'his' and sent[j - 6] == 'for':
+                    ca_set_winner += look_backward(sent, j, end=['for', 'his', 'golden', 'globe', 'win', 'as'])[:-6]
+                elif j > 6 and sent[j - 1] == 'as' and sent[j - 2] == 'win' and sent[j - 3] == 'globe' and sent[j - 4] == 'golden' and sent[j - 5] == 'her' and sent[j - 6] == 'for':
+                    ca_set_winner += look_backward(sent, j, end=['for', 'her', 'golden', 'globe', 'win', 'as'])[:-6]
 
-    sorted_ca_winner = unique_ngrams(ca_set_winner)[:90]
-    sorted_ca_winner = remove_duplicate_sublist(sorted_ca_winner)
+                # xxxx is presenting AWARD
+                if j >= 2 and sent[j - 1] == 'presenting' and sent[j - 2] == 'is':
+                    ca_set_presenter += look_backward(sent, j, end=['is', 'presenting'])[:-2]
+                # someone presents AWARD to xxx
+                elif j >= 1 and (sent[j - 1] == 'presents' or sent[j - 1] == 'presented'):
+                    ca_set_presenter += look_backward(sent, j, end=['presents'])[:-1]
+                # someone presents for AWARD
+                elif j >= 2 and sent[j - 2] == 'presents' and sent[j - 1] == 'for':
+                    ca_set_presenter += look_backward(sent, j, end=['presents', 'for'])[:-2]
 
-    # remove all sublists at this point, re-rank according to occurrence frequency
-    ca_winner = remove_all_sublists(sorted_ca_winner)
+    '''# take intersection of all
+    while [] in ca_set_winner:
+        ca_set_winner.remove([])
+    sorted_ca_winner = find_inter(ca_set_winner)[:10]'''
+    sorted_ca_freq_winners[i] = untie(unique_ngrams(ca_set_winner))
+    sorted_ca_freq_presenters[i] = untie(unique_ngrams(ca_set_presenter))
 
-    ca_freq_winner = []
-    for x in ca_awards:
+    '''ca_freq_winner = []
+    for x, _ in sorted_ca_winner:
         n = 0
         for tweet in gg2013:
             sent = tweet['text'].split()
@@ -252,5 +329,9 @@ for i, award in enumerate(awards):
                 n += 1
         ca_freq_winner.append([x, n])
 
-    sorted_ca_freq_winners[i] = sorted(ca_freq_winner, key=lambda x: x[1], reverse=True)
+    sorted_ca_freq_winners[i] = sorted(ca_freq_winner, key=lambda x: x[1], reverse=True)'''
+
+print()
+
+# find nominees for each award
 
